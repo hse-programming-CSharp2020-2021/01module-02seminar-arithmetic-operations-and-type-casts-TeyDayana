28c3f6959a704782e7f9ab6aee6c364132f695b6
Даяна Тей <ditey@edu.hse.ru>
2020-11-08 22:46:00 +0300

    is editted

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -25,19 +25,64 @@ namespace Task_02 {
 		static void Main(string[] args) {
 			int p;
 			// TODO : Реализовать ввод целочисленного значения.
-			p =;
+			p = int.Parse(Console.ReadLine());
 
-			// TODO : Посчиать и вывести результат.
+            // TODO : Посчиать и вывести результат.
+            Console.WriteLine(MaxPermutation(p));
 		}
 
 		static int MaxPermutation(int x) {
 			// TODO : Получить цифры числа используя арифметические операции.
-			int firstDigit = ,
-				secondDigit = ,
-				thirdDigit = ;
+			int firstDigit = x / 100,
+				secondDigit = x / 10 % 10,
+				thirdDigit = x % 10;
+
+			int max1, max2, min;
+			if (firstDigit > secondDigit && firstDigit > thirdDigit)
+			{
+				max1 = firstDigit;
+				if (secondDigit > thirdDigit)
+				{
+					max2 = secondDigit;
+					min = thirdDigit;
+				}
+                else
+                {
+					max2 = thirdDigit;
+					min = secondDigit;
+				}
+			}
+			else if (secondDigit > firstDigit && secondDigit > thirdDigit)
+			{
+				max1 = secondDigit;
+				if (firstDigit > thirdDigit)
+				{
+					max2 = firstDigit;
+					min = thirdDigit;
+				}
+				else
+				{
+					max2 = thirdDigit;
+					min = firstDigit;
+				}
+			}
+			else
+            {
+				max1 = thirdDigit;
+				if (firstDigit > secondDigit)
+				{
+					max2 = firstDigit;
+					min = secondDigit;
+				}
+				else
+				{
+					max2 = secondDigit;
+					min = firstDigit;
+				}
+			}
 
 			// TODO : Собрать новое число и вернуть его. 
-			int max =
+			int max = max1 * 100 + max2 * 10 + min;
 			return max;
 		}
 	}
